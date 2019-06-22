b633afa7d39752fa49ae646e20a6e9e5b6a3d544
Svet <stsupryk@gmail.com>
2019-06-23 02:53:21 +0300

    Updates

---
+++ precache-manifest.e0e15ffa05879aef9b245872d8fa9a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db17620ce1a2319ef36b48003baff53f",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "0d9c535094cf5b457332",
+    "url": "/react_static-list-of-todos/static/css/main.9491491f.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0d9c535094cf5b457332",
+    "url": "/react_static-list-of-todos/static/js/main.3467d990.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
