6f9ba011df06d5a0c34503a3eb7dc8b8c6093b4c
Svet <stsupryk@gmail.com>
2019-06-25 14:50:42 +0300

    deleted div in ul

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -4,12 +4,10 @@ import User from './User'
 function TodoItem(props) {
     const { userId, title, completed } = props;
     return (
-        <div className="todo">
-            <li className={completed ? "done" : "not"}>
-                <div className="left">{title}</div>
-                <User id={userId} />
-            </li>
-        </div>
+        <li className={completed ? "done" : "not"}>
+            <div className="left">{title}</div>
+            <User id={userId} />
+        </li>
     )
 }
 
